00c4b88968092e097870c16b6e95e6a98ae831f8
Ayush <71317507+ayushg12@users.noreply.github.com>
2021-10-24 18:50:45 +0530

    Added local discriminator

--- training/networks.py
+++ training/networks.py
@@ -972,3 +972,40 @@ class Discriminator(torch.nn.Module):
         return x
 
 #----------------------------------------------------------------------------
+persistence.persistent_class
+class LocalDiscriminator(torch.nn.Module):
+    def __init__(self, 
+        c_dim,
+        img_channels,                   # Number of input color channels.
+        img_resolution,
+        block_kwargs        = {},       # Arguments for DiscriminatorBlock.
+        mapping_kwargs      = {},       # Arguments for MappingNetwork.
+        epilogue_kwargs     = {},       # Arguments for DiscriminatorEpilogue.
+    ):
+        super().__init__()
+
+        def discriminator_block(in_filters, out_filters, normalization=True):
+            """Returns downsampling layers of each discriminator block"""
+            layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
+            if normalization:
+                layers.append(nn.InstanceNorm2d(out_filters))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *discriminator_block(img_channels, 64, normalization=False),
+            *discriminator_block(64, 128),
+            *discriminator_block(128, 256),
+            *discriminator_block(256, 512),
+            nn.ZeroPad2d((1, 0, 1, 0)),
+            nn.Conv2d(512, 1, 4, padding=1, bias=False),
+            nn.Flatten(),
+            nn.Linear(256, 1)
+        )
+
+    def forward(self, img):
+        # Concatenate image and condition image by channels to produce input
+        # img_input = torch.cat((img_A, img_B), 1)
+        return self.model(img)
+
+#----------------------------------------------------------------------------
