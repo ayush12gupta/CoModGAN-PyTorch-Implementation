568ed760c1dbe31054efbcc979407a60b086798e
ayush12gupta <gupta101a12@gmail.com>
2021-11-28 20:42:42 +0530

    add

--- training/loss.py
+++ training/loss.py
@@ -16,47 +16,47 @@ from torch_utils.ops import conv2d_gradfix
 os.environ['TORCH_HOME'] = '../pretrained/'
 
 #----------------------------------------------------------------------------
-class VGGPerceptualLoss(torch.nn.Module):
-    def __init__(self, resize=True):
-        super(VGGPerceptualLoss, self).__init__()
-        blocks = []
-        blocks.append(torchvision.models.vgg16(pretrained=True).cuda().features[:4].eval())
-        blocks.append(torchvision.models.vgg16(pretrained=True).cuda().features[4:9].eval())
-        blocks.append(torchvision.models.vgg16(pretrained=True).cuda().features[9:16].eval())
-        blocks.append(torchvision.models.vgg16(pretrained=True).cuda().features[16:23].eval())
-        for bl in blocks:
-            for p in bl.parameters():
-                p.requires_grad = False
-        self.blocks = torch.nn.ModuleList(blocks)
-        self.transform = torch.nn.functional.interpolate
-        self.resize = resize
-        self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).cuda().view(1, 3, 1, 1))
-        self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).cuda().view(1, 3, 1, 1))
+# class VGGPerceptualLoss(torch.nn.Module):
+#     def __init__(self, resize=True):
+#         super(VGGPerceptualLoss, self).__init__()
+#         blocks = []
+#         blocks.append(torchvision.models.vgg16(pretrained=True).cuda().features[:4].eval())
+#         blocks.append(torchvision.models.vgg16(pretrained=True).cuda().features[4:9].eval())
+#         blocks.append(torchvision.models.vgg16(pretrained=True).cuda().features[9:16].eval())
+#         blocks.append(torchvision.models.vgg16(pretrained=True).cuda().features[16:23].eval())
+#         for bl in blocks:
+#             for p in bl.parameters():
+#                 p.requires_grad = False
+#         self.blocks = torch.nn.ModuleList(blocks)
+#         self.transform = torch.nn.functional.interpolate
+#         self.resize = resize
+#         self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).cuda().view(1, 3, 1, 1))
+#         self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).cuda().view(1, 3, 1, 1))
 
-    def forward(self, input, target, feature_layers=[0, 1, 2, 3], style_layers=[]):
-        if input.shape[1] != 3:
-            input = input.repeat(1, 3, 1, 1)
-            target = target.repeat(1, 3, 1, 1)
-        input = (input-self.mean) / self.std
-        target = (target-self.mean) / self.std
-        if self.resize:
-            input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
-            target = self.transform(target, mode='bilinear', size=(224, 224), align_corners=False)
-        loss = 0.0
-        x = input
-        y = target
-        for i, block in enumerate(self.blocks):
-            x = block(x)
-            y = block(y)
-            if i in feature_layers:
-                loss += torch.nn.functional.l1_loss(x, y)
-            if i in style_layers:
-                act_x = x.reshape(x.shape[0], x.shape[1], -1)
-                act_y = y.reshape(y.shape[0], y.shape[1], -1)
-                gram_x = act_x @ act_x.permute(0, 2, 1)
-                gram_y = act_y @ act_y.permute(0, 2, 1)
-                loss += torch.nn.functional.l1_loss(gram_x, gram_y)
-        return loss
+#     def forward(self, input, target, feature_layers=[0, 1, 2, 3], style_layers=[]):
+#         if input.shape[1] != 3:
+#             input = input.repeat(1, 3, 1, 1)
+#             target = target.repeat(1, 3, 1, 1)
+#         input = (input-self.mean) / self.std
+#         target = (target-self.mean) / self.std
+#         if self.resize:
+#             input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
+#             target = self.transform(target, mode='bilinear', size=(224, 224), align_corners=False)
+#         loss = 0.0
+#         x = input
+#         y = target
+#         for i, block in enumerate(self.blocks):
+#             x = block(x)
+#             y = block(y)
+#             if i in feature_layers:
+#                 loss += torch.nn.functional.l1_loss(x, y)
+#             if i in style_layers:
+#                 act_x = x.reshape(x.shape[0], x.shape[1], -1)
+#                 act_y = y.reshape(y.shape[0], y.shape[1], -1)
+#                 gram_x = act_x @ act_x.permute(0, 2, 1)
+#                 gram_y = act_y @ act_y.permute(0, 2, 1)
+#                 loss += torch.nn.functional.l1_loss(gram_x, gram_y)
+#         return loss
 
 #----------------------------------------------------------------------------
 
@@ -79,7 +79,7 @@ class StyleGAN2Loss(Loss):
         self.pl_batch_shrink = pl_batch_shrink
         self.pl_decay = pl_decay
         self.pl_weight = pl_weight
-        self.vgg_loss = VGGPerceptualLoss()
+        # self.vgg_loss = VGGPerceptualLoss()
         self.pl_mean = torch.zeros([], device=device)
 
     def run_G(self, z, c, img, mask, sync):
