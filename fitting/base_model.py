9e0c49ba86f0e373ec02dc74324fd047052e1c6e
ayush12gupta <gupta101a12@gmail.com>
2022-03-20 00:58:30 +0530

    aa

--- fitting/base_model.py
+++ fitting/base_model.py
@@ -75,15 +75,6 @@ class BaseModel(ABC):
         return parser
 
     @abstractmethod
-    def set_input(self, input):
-        """Unpack input data from the dataloader and perform necessary pre-processing steps.
-
-        Parameters:
-            input (dict): includes the data itself and its metadata information.
-        """
-        pass
-
-    @abstractmethod
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         pass
