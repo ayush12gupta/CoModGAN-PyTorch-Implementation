dd0d3f6349906ed12a17bf2aa3e2c0c16e59c7f1
ayush12gupta <gupta101a12@gmail.com>
2022-05-30 05:43:56 +0530

    data read fix

--- training/dataset.py
+++ training/dataset.py
@@ -217,7 +217,7 @@ class ImageFolderDataset(Dataset):
             raise IOError('No image files found in the specified path')
 
         name = os.path.splitext(os.path.basename(self._path))[0]
-        raw_shape = [len(self._image_fnames)] + list(self._load_raw_image(0).shape)
+        raw_shape = [len(self._3dmms_fnames)] + list(self._load_3dmm_map(0).shape)
         if resolution is not None and (raw_shape[2] != resolution or raw_shape[3] != resolution):
             raise IOError('Image files do not match the specified resolution')
         super().__init__(name=name, raw_shape=raw_shape, **super_kwargs)
