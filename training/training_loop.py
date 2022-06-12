b3634feb5604b3b070b1aa377675d4f53943139c
ayush12gupta <gupta101a12@gmail.com>
2022-06-12 16:10:15 +0530

    training fix

--- training/training_loop.py
+++ training/training_loop.py
@@ -391,7 +391,7 @@ def training_loop(
 
         # Save image snapshot.
         if (rank == 0) and (image_snapshot_ticks is not None) and (done or cur_tick % image_snapshot_ticks == 0):
-            images = torch.cat([G_ema(z=z, c=c, image_in=img, mask_in=mask, noise_mode='const').cpu() for z, c, img, mask in zip(grid_z, grid_c, grid_images, grid_mask_images)]).numpy()
+            images = torch.cat([G_ema(z=z, c=c, image_in=img, mask_in=mask, noise_mode='const').cpu() for z, c, img, mask in zip(grid_z, grid_c, grid_txtr_images, grid_mask_images)]).numpy()
             save_image_grid(images, os.path.join(run_dir, f'fakes{cur_nimg//1000:06d}.png'), drange=[-1,1], grid_size=grid_size)
 
         # Save network snapshot.
